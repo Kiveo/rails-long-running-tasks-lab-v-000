89c56893e20f3efbcc2c35e30eb1a961a0367eea
Maverick Tesini <mavericktesini@gmail.com>
2018-08-20 00:29:09 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,11 @@ class SongsController < ApplicationController
   end
 
   def upload 
-    
+    CSV.foreach(params[:new_songs].path, headers: true) do |new_song|
+      @song_artist = Artist.find_or_create_by(name: new_song[1])
+      Song.create(title: new_song[0],artist_id: @song_artist.id)
+    end 
+    redirect_to songs_path
   end
 
   private
